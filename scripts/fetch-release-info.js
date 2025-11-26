8852b155060504a6c70960f83e71a77cdab7f0cb
amit-york <amitm@york.ie>
2025-11-26 13:33:38 +0530

    add space ticket eng-333

--- scripts/fetch-release-info.js
+++ scripts/fetch-release-info.js
@@ -351,6 +351,8 @@
 //       ]
 //     );
     
+
+
 //     if (updateResult.rowCount === 0) {
 //       await pool.query(
 //         `INSERT INTO current_dora_release_info (tag, ticket, release_date, project_name, environment)
